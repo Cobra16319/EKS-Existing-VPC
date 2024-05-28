6c758dbe3cd073efc09a69bc6a47c38628579f0e
Cobra <46206055+Cobra16319@users.noreply.github.com>
2024-05-28 17:16:59 -0400

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,16 +1,22 @@
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "18.30.1"  # Ensure this version or the latest compatible one
-
-  cluster_name    = var.cluster_name
-  cluster_version = var.cluster_version
-  vpc_id          = data.aws_vpc.selected.id
+resource "aws_eks_node_group" "eks_nodes" {
+  cluster_name    = module.eks.cluster_id
+  node_group_name = "eks-nodes"
+  node_role_arn   = var.node_role_arn
   subnet_ids      = data.aws_subnets.selected.ids
+
+  scaling_config {
+    desired_size = var.desired_capacity
+    max_size     = var.max_capacity
+    min_size     = var.min_capacity
+  }
+
+  instance_types = [var.instance_type]
 
   tags = {
     Environment = "govcloud"
     Terraform   = "true"
   }
 }
+
 
 
