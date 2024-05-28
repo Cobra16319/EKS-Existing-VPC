d0be337226929e9f4478ac802cf600edd5592a8a
Cobra <46206055+Cobra16319@users.noreply.github.com>
2024-05-28 16:58:47 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,26 +12,3 @@ data "aws_vpc" "selected" {
 data "aws_subnet_ids" "selected" {
   vpc_id = data.aws_vpc.selected.id
 }
-
-module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "my-eks-cluster"
-  cluster_version = "1.21"
-  subnets         = data.aws_subnet_ids.selected.ids
-  vpc_id          = data.aws_vpc.selected.id
-
-  node_groups = {
-    eks_nodes = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
-
-      instance_type = "m5.large"
-    }
-  }
-
-  tags = {
-    Environment = "govcloud"
-    Terraform   = "true"
-  }
-}
