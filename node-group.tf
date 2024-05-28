34dcf86c27277d99da25123964b14333a7854374
Cobra <46206055+Cobra16319@users.noreply.github.com>
2024-05-28 17:17:50 -0400

    Update node-group.tf

--- node-group.tf
+++ node-group.tf
@@ -1,5 +1,5 @@
 resource "aws_eks_node_group" "eks_nodes" {
-  cluster_name    = module.eks.cluster_name
+  cluster_name    = module.eks.cluster_id
   node_group_name = "eks-nodes"
   node_role_arn   = var.node_role_arn
   subnet_ids      = data.aws_subnets.selected.ids
@@ -17,4 +17,5 @@ resource "aws_eks_node_group" "eks_nodes" {
     Terraform   = "true"
   }
 }
+
 
